d23ab4c5f9f2b190161dc3bcf57e1e676eaa65b2
rmnjaat <ramanjangu01@outlook.com>
2024-01-11 20:47:29 +0530

    Handle added

--- src/components/HeroSection/About.js
+++ src/components/HeroSection/About.js
@@ -1,6 +1,11 @@
 import React from "react";
 import photo from './Images/img4.jpeg';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+
 import './About.css'
 
 
@@ -17,7 +22,39 @@ function ABOUT() {
                         <button type="button">Dowanload CV</button>
                     </div>
 
+
+                    <div className="social">
+
+                        <div className="handles">
+                            <div className="handle linkdin">
+                                <a href="https://www.linkedin.com/in/raman-jangu" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faLinkedin} size="1x" />
+                                </a>
+                            </div>
+                            <div className="handle Github">
+                                <a href="https://github.com/rmnjaat" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faGithub} size="1.5x" />
+                                </a>    
+                            </div>
+                            <div className="handle Gmail">
+                                <a href="mailto:ramanjangu01@outlook.com" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faEnvelope} size="1x" />
+                                </a>
+                            </div>
+                            <div className="handle instagram">
+                                <a href="https://www.instagram.com/rmn_jangu_jaat" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faInstagram} size="1x" />
+                                </a>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
                 </div>
+
+
                 <div className="picture">
                     <img src={photo} alt=" photo" />
                 </div>
