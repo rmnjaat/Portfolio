91c4a28584e493ee194cc30922c9ac6f6354b0d7
rmnjaat <ramanjangu01@outlook.com>
2024-01-18 18:08:26 +0530

    Skill Section added

---
+++ src/components/Skills/skill.js
@@ -0,0 +1,310 @@
+import React from "react";
+import './skill.css';
+
+
+function skill() {
+    return (
+        <>
+            <div className="skill-main-container">
+                <div className="heading">
+                    <h1>Skills</h1>
+                    <div className="line"></div>
+                </div>
+
+                <div className="Boxes">
+                    <div className="Box">
+                        <div className="technical">
+
+                            <h1>Languages</h1>
+                        </div>
+                        <div className="list">
+
+                            
+
+                            <div className="list-cont">
+                                <div className="list-img">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" />
+                                </div>
+
+                                <div className="list-name">
+                                    C
+                                </div>
+
+                            </div>
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    C<span className="pp">++</span>
+                                </div>
+
+                            </div>
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    Java
+                                </div>
+
+                            </div>
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+
+
+                                </div>
+
+                                <div className="list-name">
+                                    Python
+                                </div>
+
+                            </div>
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" />
+
+
+                                </div>
+
+                                <div className="list-name">
+                                    Bash
+                                </div>
+
+                            </div>
+
+                            <div className="list-cont">
+                                <div className="list-img">
+          
+                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" />
+          
+
+                                </div>
+
+                                <div className="list-name">
+                                    MySql
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div className="Box">
+
+                        <div className="technical">
+                            <h1> FrontEnd</h1>
+                        </div>
+
+
+                        <div className="list">
+
+
+                        <div className="list-cont">
+                                <div className="list-img">
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    Html
+                                </div>
+
+                            </div>
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    Css
+                                </div>
+
+                            </div>
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    Javascript
+                                </div>
+
+                            </div>
+
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    Bootstrap
+                                </div>
+
+                            </div>
+
+
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    React
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="Box">
+                        <div className="technical">
+
+                            <h1> Other</h1>
+                        </div>
+                        <div className="list">
+
+                            
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/windows8/windows8-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    Windows
+                                </div>
+
+                            </div>
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" />
+
+                                </div>
+
+                                <div className="list-name">
+                                    Linux
+                                </div>
+
+                            </div>
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+
+                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg" />
+          
+                                </div>
+
+                                <div className="list-name">
+                                    Oracle
+                                </div>
+
+                            </div>
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+
+                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+          
+                                </div>
+
+                                <div className="list-name">
+                                    Figma
+                                </div>
+
+                            </div>
+
+                           
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+
+                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+          
+                                </div>
+
+                                <div className="list-name">
+                                    Git
+                                </div>
+
+                            </div>
+                            
+
+                            <div className="list-cont">
+                                <div className="list-img">
+
+
+
+                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+          
+                                </div>
+
+                                <div className="list-name">
+                                    Github
+                                </div>
+
+                            </div>
+
+                            
+
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+        </>
+    )
+}
+
+
+export default skill;
